83d96d523c73e39df2c9f29f9646f923e8274061
denysnovoascmspain <denys.novoa@scmspain.com>
2017-04-25 16:20:28 +0200

    rename to movies

--- app/src/main/java/com/denysnovoa/nzbmanager/radarr/movies/view/screen/MoviesActivity.kt
+++ app/src/main/java/com/denysnovoa/nzbmanager/radarr/movies/view/screen/MoviesActivity.kt
@@ -1,11 +1,12 @@
-package com.denysnovoa.nzbmanager
+package com.denysnovoa.nzbmanager.radarr.movies.view.screen
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.denysnovoa.nzbmanager.R
 
 class MoviesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movies)
     }
 }
