cfa7491c8f978bb39c40e13dead5eaae696853e7
denysnovoascmspain <denys.novoa@scmspain.com>
2017-04-25 13:25:28 +0200

    remove name raddar from view and presenter

--- app/src/main/java/com/denysnovoa/nzbmanager/MoviesActivity.kt
+++ app/src/main/java/com/denysnovoa/nzbmanager/MoviesActivity.kt
@@ -3,7 +3,7 @@ package com.denysnovoa.nzbmanager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MoviesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/denysnovoa/nzbmanager/radarr/movies/view/MoviesPresenter.kt
+++ app/src/main/java/com/denysnovoa/nzbmanager/radarr/movies/view/MoviesPresenter.kt
@@ -1,6 +1,6 @@
 package com.denysnovoa.nzbmanager.radarr.movies.view
 
-class RadarrMoviesPresenter {
+class MoviesPresenter {
 
 
 }
