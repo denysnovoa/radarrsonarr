f2f1fa45d0ca2230a1efc5f8a631b40d56dbc2ba
denysnovoascmspain <denys.novoa@scmspain.com>
2017-05-01 18:08:28 +0200

    add log in firebase crash

--- app/src/main/java/com/denysnovoa/nzbmanager/common/framework/ErrorLog.kt
+++ app/src/main/java/com/denysnovoa/nzbmanager/common/framework/ErrorLog.kt
@@ -1,8 +1,11 @@
 package com.denysnovoa.nzbmanager.common.framework
 
 import android.util.Log
+import com.google.firebase.crash.FirebaseCrash
 
 class ErrorLog {
-    fun log(error: Throwable?) { Log.e("ErrorLog", "Error", error)
+    fun log(error: Throwable?) {
+        Log.e("ErrorLog", "Error", error)
+        FirebaseCrash.report(error)
     }
 }
