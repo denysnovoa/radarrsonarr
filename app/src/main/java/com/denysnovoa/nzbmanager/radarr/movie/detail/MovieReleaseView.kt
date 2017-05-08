e3eb2b7cdc3ba9ecfc486796575580f985c26add
denysnovoascmspain <denys.novoa@scmspain.com>
2017-05-08 11:42:32 +0200

    delete duplicate release files

--- app/src/main/java/com/denysnovoa/nzbmanager/radarr/movie/detail/MovieReleaseView.kt
+++
@@ -1,8 +0,0 @@
-package com.denysnovoa.nzbmanager.radarr.movie.detail
-
-import com.denysnovoa.nzbmanager.radarr.movie.detail.view.model.MovieReleaseViewModel
-
-interface MovieReleaseView {
-    fun showMovieReleases(movieReleases: List<MovieReleaseViewModel>)
-    fun showErrorSearchReleases()
-}
--- app/src/main/java/com/denysnovoa/nzbmanager/radarr/movie/detail/repository/mapper/MovieReleaseMapper.kt
+++
@@ -1,8 +0,0 @@
-package com.denysnovoa.nzbmanager.radarr.movie.detail.repository.mapper
-
-import com.denysnovoa.nzbmanager.radarr.movie.detail.repository.MovieReleaseEntity
-import com.denysnovoa.nzbmanager.radarr.movie.detail.repository.model.MovieReleaseModel
-
-interface MovieReleaseMapper {
-    fun transform(movie: MovieReleaseEntity): MovieReleaseModel
-}
