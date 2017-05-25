0098e7aa20baa11da3a34f87f38009489b9cb662
denysnovoa <dnovoa20@gmail.com>
2017-05-25 09:53:27 +0200

    add Repository Movies Offline Mode

--- app/src/main/java/com/denysnovoa/nzbmanager/radarr/movie/list/repository/api/RadarrMoviesApiClient.kt
+++ app/src/main/java/com/denysnovoa/nzbmanager/radarr/movie/list/repository/api/RadarrMoviesApiClient.kt
@@ -1,16 +1,23 @@
 package com.denysnovoa.nzbmanager.radarr.movie.list.repository.api
 
+import com.denysnovoa.nzbmanager.common.framework.OfflineMode
 import com.denysnovoa.nzbmanager.radarr.movie.list.repository.mapper.MoviesMapper
 import com.denysnovoa.nzbmanager.radarr.movie.list.repository.model.MovieModel
 import io.reactivex.Flowable
 import io.reactivex.Single
 
+
 class RadarrMoviesApiClient(val moviesApi: RadarrMoviesApiRest, val movieMapper: MoviesMapper) : MoviesApiClient {
-    override fun getMovies(): Flowable<List<MovieModel>> = moviesApi.movies()
-            .flatMapIterable { it }
-            .map(movieMapper::transform)
-            .toList()
-            .toFlowable()
+    override fun getMovies(): Flowable<List<MovieModel>> =
+            if (OfflineMode) {
+                Flowable.empty()
+            } else {
+                moviesApi.movies()
+                        .flatMapIterable { it }
+                        .map(movieMapper::transform)
+                        .toList()
+                        .toFlowable()
+            }
 
     override fun getDetail(id: Int): Single<MovieModel> = moviesApi.getDetail(id)
             .map(movieMapper::transform)
