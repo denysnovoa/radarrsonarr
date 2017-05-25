d5e159d9e005ee4ba09e51b46df714f76b44ac4e
Denys Novoa <dnovoa20@gmail.com>
2017-05-25 21:01:51 +0200

    Add offline mode (#5)
    * add flavor offline
    
    * add Repository Movies Offline Mode
    
    * add movie detail and release json
    
    * change to OfflineJsonProvider
    
    * add offline to releases
    
    * add offline Detail json

--- app/src/main/java/com/denysnovoa/nzbmanager/common/framework/AppConfiguration.kt
+++ app/src/main/java/com/denysnovoa/nzbmanager/common/framework/AppConfiguration.kt
@@ -4,3 +4,4 @@ val ApiUrl = "http://dnovoa20.ddns.net:7878/"
 val ApiKey = "b5536e00243a4fd9ad002c53202fb771"
 val ApiCacheKey = "http-cache"
 val PicassoCacheKey = "http-cache-picasso"
+val OfflineDebugMode = true
