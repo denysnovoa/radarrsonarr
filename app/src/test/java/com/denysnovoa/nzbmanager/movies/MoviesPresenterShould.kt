7048c1d70feed80ea69cfb1370a8b86f3a778f9c
denysnovoa <dnovoa20@gmail.com>
2017-07-08 14:07:36 +0200

    test show error api

--- app/src/test/java/com/denysnovoa/nzbmanager/movies/MoviesPresenterShould.kt
+++ app/src/test/java/com/denysnovoa/nzbmanager/movies/MoviesPresenterShould.kt
@@ -2,16 +2,15 @@ package com.denysnovoa.nzbmanager.movies
 
 import com.denysnovoa.nzbmanager.common.framework.ErrorLog
 import com.denysnovoa.nzbmanager.radarr.movie.list.domain.GetLastMoviesUseCase
+import com.denysnovoa.nzbmanager.radarr.movie.list.repository.api.ApiUnknownHostException
 import com.denysnovoa.nzbmanager.radarr.movie.list.view.MoviesView
 import com.denysnovoa.nzbmanager.radarr.movie.list.view.mapper.MoviesViewMapper
 import com.denysnovoa.nzbmanager.radarr.movie.list.view.presenter.MoviesPresenter
 import com.denysnovoa.nzbmanager.settings.screen.domain.GetRadarrSettingsUseCase
-import com.denysnovoa.nzbmanager.settings.screen.repository.model.RadarrSettingsModel
-import io.reactivex.Single
+import io.reactivex.Flowable
 import io.reactivex.schedulers.Schedulers
 import org.junit.Before
 import org.junit.Test
-import org.mockito.BDDMockito.given
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.Mockito.verify
@@ -40,12 +39,14 @@ class MoviesPresenterShould {
 
     @Test
     fun show_message_to_configure_api_when_not_have_api_storage() {
-        given(getRadarrSettingsUseCase.get()).willReturn(Single.just(RadarrSettingsModel("", 7878, "")))
+        val apiError = ApiUnknownHostException("Api error")
+        Mockito.`when`(getLastMoviesUseCase.get()).thenReturn(Flowable.error(apiError))
 
         presenter.onResume()
 
         verify(view).showConfigureApi()
-        verify(getLastMoviesUseCase, Mockito.never()).get()
+        verify(view).hideLoading()
+        verify(errorLog).log(apiError)
     }
 
     @Before
